8c1af208dda17c4f3049ae1c0911c069bcd39e41
Vinicius <143036118+ViniciusCampas@users.noreply.github.com>
2025-10-07 00:17:53 +0000

    Primeira-Aula

---
+++ tarefas/classes/Tarefas.cs
@@ -0,0 +1,11 @@
+public class Tarefas{
+    //Shift+alt para selecionar a coluna
+    //ctrl+shift seleciona o conjunto
+    public int id { get; set; }
+    public string nome { get; set; }
+    public string descricao { get; set; }
+               public DateTime dataCriacao { get; set; }
+    public int status { get; set; }
+    public DateTime dataExecucao { get; set; }
+
+}
